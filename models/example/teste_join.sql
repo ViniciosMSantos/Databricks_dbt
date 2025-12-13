921b4f3e4c23b5516fb9241bbd5c23ccdcc353c9
Vinicios Maciel <viniciosmacielsantos99@gmail.com>
2025-12-13 17:51:53 -0300

    Ajuste

--- models/example/teste_join.sql
+++ models/example/teste_join.sql
@@ -1,9 +1,7 @@
 {{ config(materialized='table', schema='gold') }}
 
-select 
+select
     *
-
-from {{ ref('tb_orders_bronze') }} as orders
-    
-    left join {{ ref('tb_order_items_bronze') }} as order_items
-        on orders.order_id = order_items.order_id
+from {{ source('bakehouse', 'tb_orders_bronze') }} as orders
+left join {{ source('bakehouse', 'tb_order_items_bronze') }} as order_items
+    on orders.order_id = order_items.order_id
