947df999ab3645cd635274127970d070d2eaa650
Vinicios Maciel <viniciosmacielsantos99@gmail.com>
2025-12-13 17:58:41 -0300

    Ajuste Base

--- models/example/teste_join.sql
+++ models/example/teste_join.sql
@@ -1,7 +1,8 @@
-{{ config(materialized='table', schema='gold') }}
+{{ config(materialized='table') }}
 
 select
     *
-from {{ source('bakehouse', 'tb_orders_bronze') }} as orders
-left join {{ source('bakehouse', 'tb_order_items_bronze') }} as order_items
+from {{ source('olist_src', 'tb_orders_bronze') }} as orders
+left join {{ source('olist_src', 'tb_order_items_bronze') }} as order_items
     on orders.order_id = order_items.order_id
+    
