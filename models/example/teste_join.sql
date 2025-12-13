a8964be2c06c5c6ed20be8851f42518874bf1082
Vinicios Maciel <viniciosmacielsantos99@gmail.com>
2025-12-13 18:01:24 -0300

    Ajuste fino

--- models/example/teste_join.sql
+++ models/example/teste_join.sql
@@ -1,8 +1,18 @@
 {{ config(materialized='table') }}
 
 select
-    *
+    orders.order_id,
+    orders.customer_id,
+    orders.order_status,
+    orders.order_purchase_timestamp,
+
+    order_items.order_item_id,
+    order_items.product_id,
+    order_items.price,
+    order_items.freight_value
+
 from {{ source('olist_src', 'tb_orders_bronze') }} as orders
 left join {{ source('olist_src', 'tb_order_items_bronze') }} as order_items
     on orders.order_id = order_items.order_id
-    
+
+
